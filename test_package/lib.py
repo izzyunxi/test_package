2344b51c3ab56a34b659bb7641dd97dcd4f9aab8
Yunxi Zhang <zhang.yunxi.cn@gmail.com>
2020-11-16 15:16:00 +0100

    try_me modified

--- test_package/lib.py
+++ test_package/lib.py
@@ -47,6 +47,8 @@ def clean_data(data):
     data.loc[:, 'Frequency'] = data['Frequency'].map(drows)
     return data
 
+def try_me():
+    return "Have you done with all flashcards:)"
 
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
